8df0ff2148b0fc11c0ebaa7f338db6a6a4f08673
zhouyongran <zhouyongran@ppmoney.com>
2020-08-31 22:34:53 +0800

    feat:创建项目

---
+++ src/main/java/com/framework/venus/VenusApplication.java
@@ -0,0 +1,13 @@
+package com.framework.venus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/framework/venus/VenusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.venus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
